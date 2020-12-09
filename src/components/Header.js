857999654863bc9873e25a5779a2a012c94aff40
Ady <ady@gms-macbook-pro.lan>
2020-12-09 10:39:44 -0800

    redeploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,18 @@ import headerImg from '../root/header-img.jpg';
 
 export default class Header extends React.Component {
     render() {
-        const imageUrl = window.innerWidth <= 500 ? headerImg : '';
-        return (
+        const imageUrl = window.innerWidth <= 500 ? headerImg : headerImg;
+        return (<div style={{backgroundColor: '#e0e5e9'}}>
             <div 
                 id='header'
                 className="App" 
                 style={{
-                    backgroundImage: `url(${imageUrl})`
+                    backgroundImage: `url(${imageUrl})`,
+                    width: '40%',
+                    height: '200px'
                 }}>
                 <h1>Petful <FaPaw style={{fontSize:'33px'}} /></h1> 
+            </div>
             </div>
         );
     }
